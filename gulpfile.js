4f0c9fcd17ae0d5915832a11849d3e83e22603a2
Aleksandr Sobol <bonpet666@gmail.com>
2015-01-25 16:58:14 +0300

    настроен для работы файл gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,7 @@
 
 
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
-var jshint = require('gulp-jshint');
+var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
 var reload  = browserSync.reload;
 
@@ -24,16 +18,17 @@ gulp.task('browser-sync', function() {
 // Sass task, will run when any SCSS files change & BrowserSync
 // will auto-update browsers
 gulp.task('sass', function () {
-    return gulp.src('./app/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./app/css'))
+    return gulp.src('./app/styles/scss/*.scss')
+        .pipe($.sass())
+        .pipe(gulp.dest('./app/styles'))
         .pipe(reload({stream:true}));
 });
 
 // process JS files and return the stream.
 gulp.task('js', function () {
     return gulp.src('./app/scripts/*.js')
-        .pipe(uglify())
+        .pipe($.concat())
+        .pipe($.uglify())
         .pipe(gulp.dest('dist/js'));
 });
 
@@ -43,8 +38,8 @@ gulp.task('html', function(){
 });
 
 // Default task to be run with `gulp`
-gulp.task('default', ['sass', 'browser-sync'], function () {
-    gulp.watch("app/scss/*.scss", ['sass']);
+gulp.task('serve', ['sass', 'browser-sync'], function () {
+    gulp.watch("app/styles/scss/*.scss", ['sass']);
     gulp.watch('./app/*.html', ['html']);
     gulp.watch("js/*.js", ['js', browserSync.reload]);
 });
