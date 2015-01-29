7fd2ce01d69f4abd371065a1568ef01852aab935
Aleksandr Sobol <bonpet666@gmail.com>
2015-01-29 19:55:19 +0300

    added favicon

--- dist/tmpl/head.php
+++ dist/tmpl/head.php
@@ -5,5 +5,6 @@
 	<!--[if IE]>
 	<script src="scripts/html5shiv.js"></script>
 	<![endif]-->
+	<link rel="icon" type="image/vnd.microsoft.icon" href="images/favicon.ico" />
 	<link rel="stylesheet" href="styles/styles.css">
 </head>
